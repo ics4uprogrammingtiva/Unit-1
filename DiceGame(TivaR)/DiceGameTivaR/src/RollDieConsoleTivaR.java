607fc8f4fbb1887138a307fb8b6b2745e8d82d63
TivaR8 <tiva.rait@ocsbstudent.ca>
2019-02-22 13:08:02 -0500

    Started Roll Die Console

---
+++ DiceGame(TivaR)/DiceGameTivaR/src/RollDieConsoleTivaR.java
@@ -0,0 +1,25 @@
+/*
+ * Created by: Tiva Rait
+ * Created on: 22-Feb-2019
+ * Created for: ICS4U
+ * Daily Assignment – Day #9 Roll Die Console
+ * This program will randomly generate a value between...
+ * ... 1 and maxValue and place the value in the console.
+*/
+public class RollDieConsoleTivaR {
+
+	public static void main(String[] args) {
+		// Variables
+		
+		// Put Procedure here
+		
+		
+		
+		
+		
+		// Get the maxValue from the user
+		
+
+	}
+
+}
