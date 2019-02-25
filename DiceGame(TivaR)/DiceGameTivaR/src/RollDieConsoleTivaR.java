bf71081f0a7d944a07eeffcf78af894c7207db8c
TivaR8 <tiva.rait@ocsbstudent.ca>
2019-02-25 13:04:29 -0500

    Update of Roll Die Console

--- DiceGame(TivaR)/DiceGameTivaR/src/RollDieConsoleTivaR.java
+++ DiceGame(TivaR)/DiceGameTivaR/src/RollDieConsoleTivaR.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /*
  * Created by: Tiva Rait
  * Created on: 22-Feb-2019
@@ -6,20 +7,46 @@
  * This program will randomly generate a value between...
  * ... 1 and maxValue and place the value in the console.
 */
+
+// Not finished this day work on it more at home. 
+// Need to get real random number and I have to add a try catch
 public class RollDieConsoleTivaR {
 
 	public static void main(String[] args) {
 		// Variables
+		int maxValue;
+		int minValue;
 		
-		// Put Procedure here
+		// To get users answer from the console
+		Scanner input = new Scanner(System.in);
+		
+		// Ask question in the console
+		System.out.println("Please enter a max value for the random number:");
+		// Get the answer from the console
+		maxValue = input.nextInt();
+		
+		// Ask question to get minimum value
+		System.out.println("Please enter a min value for the random number");
+		// Get answer from console
+		minValue = input.nextInt();
 		
 		
+		int randomNumber = RollDie(minValue, maxValue);
 		
 		
+		// Display the random number to the user
+		System.out.println("Random number is: "+ randomNumber);
 		
-		// Get the maxValue from the user
-		
+		// Close the input
+		input.close();
 
 	}
+	
+	// Put Procedure here
+			public static int RollDie( int minValue, int maxValue)
+			{
+				int randomNumber = 1; 
+				return randomNumber;
+			}
 
 }
